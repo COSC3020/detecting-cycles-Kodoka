92ba141d129b4d6bc8fd04bbf97f61f39e4f7177
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-16 15:49:37 -0600

    Push to work on at home.

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
-function hasCycle(graph) {
-    return false;
+// Statuses: 0 = unvisited, 1 = visiting, 2 = visited.
+
+function hasCycle(graph)
+{
+    var vertexSet = new Set();
+    var verticeStatus = {};
+    var visitedVertexSet = new Set();
+
+    // For each edge, add the unique vertices that it connects to a set.
+    for(var i = 0; i < graph.length; i++)
+    {
+        vertexSet.add(graph[i][0]);
+        vertexSet.add(graph[i][1]);
+    }
+
+    // Add each unique vertex to key/value set, with an initial status of 0.
+    for(var i = 0; i < vertexSet.size; i++)
+    {
+        verticeStatus[i] = 0;
+    }
 }
